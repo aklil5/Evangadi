b5fe45996087073e534e98acdef2734f846bf234
aklil5 <aklilezewdu@gmail.com>
2026-06-15 23:03:09 +0300

    Phase-2 Week-12 DOM and JS Events

---
+++ Phase-2js/Week_12_DOMandEvents/DOM-exercise-file/main.js
@@ -0,0 +1,30 @@
+//  your solutions start here
+//  make sure to connect your main.js file with your html 
+// happy coding 🧑‍💻
+
+// var document = {
+//     html: {
+//         head: {
+//             title: {
+//                 value: "The title"
+//             }
+//         }
+//     }
+// }
+// console.log(document.html.head.title.value); //The title
+
+// document.html.head.title.value = "Another title"
+
+// console.log(document.html.head.title.value); //Another title
+
+
+let red = document.getElementById("one")
+
+console.log(red)
+
+
+let blue = document.querySelector(".red");
+blue.className = "blue"
+
+
+console.log(document.querySelectorAll(".red"));
