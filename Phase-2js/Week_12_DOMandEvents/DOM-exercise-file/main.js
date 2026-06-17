5331672b5d915bb3d6d6b4988de1f10477c5b615
aklil5 <aklilezewdu@gmail.com>
2026-06-18 00:42:14 +0300

    practicing altering values in DOM and  binding an event in different ways

--- Phase-2js/Week_12_DOMandEvents/DOM-exercise-file/main.js
+++ Phase-2js/Week_12_DOMandEvents/DOM-exercise-file/main.js
@@ -85,16 +85,199 @@
 
 
 
-// ===================================
-// Traversing between multiple elements
+// // ===================================
+// // Traversing between multiple elements
 
 
-let tanash = document.querySelector(".red").nextElementSibling
+// let tanash = document.querySelector(".red").nextElementSibling
 
-console.log(tanash);
+// console.log(tanash);
 
-let welaj = document.querySelector(".blue").parentNode
-console.log(welaj);
+// let welaj = document.querySelector(".blue").parentNode
+// console.log(welaj);
 
-let lj = document.getElementById("techCompanies").firstElementChild
-console.log(lj);
+// let lj = document.getElementById("techCompanies").firstElementChild
+// console.log(lj);
+
+
+
+// =====================================
+
+
+
+// let para = document.getElementById("firstPar")
+
+// para.textContent = "iPhoone"
+
+
+// let newPar = document.createElement("p")
+
+// newPar.textContent = "Here is another paragraph"
+
+// document.getElementById("sample1").prepend(newPar)
+
+
+// let liEml = document.createElement("li")
+// liEml.innerText = "Avocado"
+
+// document.getElementById("techCompanies").prepend(liEml)
+
+
+// let parent = document.getElementById("techCompanies")
+// let tobeRemoved = document.getElementById("lastElem")
+
+// parent.removeChild(tobeRemoved)
+
+
+
+// let referencePt = document.getElementById("micro")
+
+// let newElem = document.createElement("li")
+// newElem.textContent = "the Samsung Family"
+
+// referencePt.after(newElem)
+
+
+
+// let samsungg = document.getElementById("sam")
+
+// console.log(samsungg.className)
+
+// samsungg.classList.add("yellowBg")
+
+// console.log(samsungg.className)
+
+// samsungg.classList.toggle("yellowBg")
+
+// samsungg.classList.toggle("yellowBg")
+
+// let samsung = document.getElementById("sam")
+
+// console.log(samsung.hasAttribute("class"))
+
+// console.log(samsung.getAttribute("class"))
+
+// samsung.setAttribute("class", "red")
+
+
+let companies = document.getElementById("techCompanies")
+
+companies.style.backgroundColor = "yellow"
+companies.style.color = "white"
+companies.style.fontSize = "40px"
+companies.style.border = "purple double 10px"
+companies.style.display = "none"
+companies.style.display = "block"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ============================================
+// Ways to Bind an event
+// 1. HTML event handlers
+
+
+// function alertMes(){
+//     alert("hi there clicker")
+// }
+
+function changeBg(){
+    document.body.style.backgroundColor = "purple"
+}
+
+function removeBG(){
+    document.body.style.backgroundColor = ""
+}
+
+// let yesButton = document.getElementById("yes")
+// yesButton.onclick = changeBg
+
+// yesButton.ondblclick = removeBG
+
+
+// document.getElementById("yes").addEventListener("dblclick", changeBg)
+
+
+
+// yesBtn.addEventListener("click", function() {
+//     document.body.style.backgroundColor = "green"
+// })
+
+// let linkedTag = document.getElementById("nextSite")
+
+// linkedTag.addEventListener("click", function(e) {
+//     e.preventDefault()
+//     linkedTag.textContent = "Not loading the new site, but showing you some text."
+// })
+
+
+
+
+// =====================================
+// Form Validation
+
+let nameCheck = document.getElementById("adder")
+
+nameCheck.onsubmit = formSubmitChecker;
+
+function formSubmitChecker(e){
+    
+
+    let errors = [];
+
+    let elErrorsDisplay = document.getElementById("errorsDisplay");
+    elErrorsDisplay.innerHTML = "";
+    
+
+    let elFirstName = document.getElementsByName("first-value")
+
+    let elAnotherName = document.getElementsByName("second-value");
+
+    let firstNameVal = elFirstName[0].value
+    let secondNameVal = elAnotherName[0].value
+    
+    if (!firstNameVal) {
+        errors.push("First value is empty")
+    } else if (secondNameVal.length < 7) {
+        errors.push("second val not > 7 characters.")
+    } else {
+
+    }
+
+    
+    if (errors.length > 0) {
+        e.preventDefault();
+        elErrorsDisplay.style.display = "block"
+        for (let i = 0; i < errors.length; i++) {
+            elErrorsDisplay.innerHTML += errors[i] + "<br>";
+        } else {
+            alert("Submitted")
+        }
+
+    }
+}
