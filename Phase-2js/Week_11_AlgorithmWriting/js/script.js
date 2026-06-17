9058618b15f29e501bc00cf547e90ebdcd8e6dca
aklil5 <aklilezewdu@gmail.com>
2026-06-17 07:45:22 +0300

    more on advanced algorithm writing

--- Phase-2js/Week_11_AlgorithmWriting/js/script.js
+++ Phase-2js/Week_11_AlgorithmWriting/js/script.js
@@ -129,6 +129,58 @@
 // So {21, 3, 7, 9, 11, 4, 6} is a Magic array because 3, 7, 11 are the primes in the array and they sum to 21 which is the first element of the array. {13, 4, 4, 4, 4} is also a Magic array because the sum of the primes is 13 which is also the first element. Other Magic arrays are {10, 5, 5}, {0, 6, 8, 20} and {3}. {8, 5, -5, 5, 3} is not a Magic array because the sum of the primes is 5+5+3 = 13. Note that -5 is not a prime because prime numbers are positive.
 
 
+
+// FIRST
+// - CHECK IF PRIME OR NOT WITH A FUNCTION
+// - isPrime function - if less than 1 - false
+//   - if 2 true
+//   - if divisible by 2 - false
+//   - for loop starting from 3 to sqrt(num) - divisible by i - false
+//   - return true
+
+// function isPrime(num) {
+//     if (num <= 1) return false;
+//     if (num === 2) return true;
+//     if (num % 2 == 0) return false;
+
+//     for (let i = 3; i<= Math.sqrt(num); i+=2) {
+//         if (num % i === 0) return false;
+//     }
+//     return true;
+// }
+
+// // console.log(isPrime(15));
+// // console.log(isPrime(7));
+
+
+// function isMagic(a) {
+//     let sum = 0;
+//     // Start i at 1 so we don't accidentally include the target number a[0] in the sum
+//     for (let i=1; i<a.length; i++) {
+//         let num = a[i];
+//         if (isPrime(num)) {
+//             sum = sum + num
+//         }
+//     }
+
+//     if (sum === a[0]) {
+//         return "Magic"
+//     } else {
+//         return "Not Magic"
+//     }
+// }
+// console.log(isMagic([21, 3, 7, 9, 11, 4, 6])) //is a Magic array 
+// console.log(isMagic([13, 4, 4, 4, 4])) //not
+
+
+
+
+
+
+// ===================================================
+// ===================================================
+
+
 // PSEUDO CODE
 // define a function "magicArray"
 // if 0, 1, -ve, or even -  not prime
@@ -171,33 +223,134 @@
 
 
 
+// FIXXXXXXXXXXXXX
+
+// function isMagic(a) {
+//     let sumPrime = 0;
+    
+//     // FIX 1: Start at index 1 to skip the target total, and use a.length
+//     for (let i = 1; i < a.length; i++) {
+//         let num = a[i];
+        
+//         // FIX 2: Skip invalid numbers instead of returning false
+//         if (num <= 1) continue; 
+//         if (num === 2 || num === 3) {
+//             sumPrime += num;
+//             continue;
+//         }
+//         if (num % 2 === 0 || num % 3 === 0) continue;
+        
+//         // FIX 3: Assume it is prime until proven otherwise
+//         let isPrime = true;
+//         // Optimization: Checking up to the square root is much faster than num / 2
+//         let limit = Math.sqrt(num); 
+        
+//         for (let j = 5; j <= limit; j += 6) {
+//             if (num % j === 0 || num % (j + 2) === 0) {
+//                 isPrime = false;
+//                 break; // Found a factor! Stop checking.
+//             }
+//         }
+        
+//         // Only add to the sum if it survived the whole check
+//         if (isPrime) {
+//             sumPrime += num;
+//         }
+//     }
+    
+//     // FIX 4: Clean comparison block
+//     if (sumPrime === a[0]) {  
+//         console.log("Magic");
+//     } else {
+//         console.log("Not Magic");
+//     }
+// }
+
+// isMagic([21, 3, 7, 9, 11, 4, 6]); // Output: Magic
+// isMagic([13, 4, 4, 4, 4]);        // Output: Not Magic
 
 
 
+
+
+// BRUKEs approach
+
+// function isMagic(a){
+// 	let isPrime = true;
+// 	let sum = 0;
+// 	for (i=0; i< a.length - 1; i++) {
+// 		let num = a[i]
+// 		if (num < 2) isPrime = false;
+// 		for (j=2; j < num/2 + 1; j ++) {
+// 			if (num % j == 0) isPrime = false;
+// 		}
+// 		if (isPrime) {
+// 			sum = sum + num
+// 		}
+// 	}
+// 	if (sum === a[0]) {
+// 		return "Magic Array"
+// 	} else {
+// 		return "Not Magic"
+// 	}
+	
+// }
+
+// console.log(isMagic([21, 3, 7, 9, 11, 4, 6])) //is a Magic array
+
+// console.log(isMagic([13, 4, 4, 4, 4])) //not
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======================================================
+// ===================================================
 
 // 44444444444
 // Sort function 
 
 // PSEUDO CODE
 // Declre a function - sorttt that accepts array of argt
-// have smallvalue vr declared
-// going through the array , check if tht element is is the smllest with nested loop 
+// going through the array , check if the first element is greater than the second - if so change their position
+//  with temporary value = first element
+// first element = second element
+// second element = temporary element 
 
 
 function sorttt(a){
-    let smallvar = 0;
-    for (let i = 0; i < a.length; i++) {
-        let firstVal = a[i];
-        for (let j=1; j < a.length; j++) {
-            let secondVal = [j]
-            if (firstVal < secondVal) {
-                
+    let checker = true;
+    while (checker) {
+        checker = false;
+        for (let i=0; i<a.length - 1; i++) {
+        // we want to stop at length - 2
+            if (a[i] > a[i+1]) {
+                checker = true;
+                let temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
             }
         }
     }
+
+    return a;
 }
 
 
 
-([21, 3, 7, 9, 11, 4, 6])  
-([13, 4, 4, 4, 4]) 
+console.log(sorttt(([21, 3, 7, 9, 11, 4, 6])))  
+console.log(sorttt(([13, 4, 4, 4, 4])))
