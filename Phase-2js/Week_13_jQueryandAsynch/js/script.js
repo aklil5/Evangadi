b60636496984bc0a37f5599d3fe148c869f75ac9
aklil5 <aklilezewdu@gmail.com>
2026-06-26 16:34:16 +0300

    jquery practices using different methods and properties

---
+++ Phase-2js/Week_13_jQueryandAsynch/js/script.js
@@ -0,0 +1,131 @@
+// let elem = $("#adder")
+// console.log(elem);
+
+// let inputLists = $("#formID input")
+// console.log(inputLists);
+
+// inputLists.first().css("background-color", "pink")
+
+// // $("input").last().hide()
+// $("input:last").hide()
+// // $("input:last").show()
+
+// $("input:visible").css("background-color", "green")
+// $("input:even").css("background-color", "purple")
+
+
+// // inputLists.last().empty()
+
+
+
+// // let classs = $(".form-values")
+
+// // $("p:nth-child(2)").hide()
+
+// $("p:nth-child(2)").has("input")
+// $("p:contains('First value')").css("background-color", "pink")
+
+// $("button[id=submitButton]").hide()
+
+
+// $("#adder div").text("helo") 
+
+// let a = $("p").text("adada")
+
+// console.log(a)
+
+
+// let aa = $("#adder").html("<li>tungtung</li>")
+// console.log(aa);
+
+// let inputs = $("#formID")
+// inputs.prepend("<input placeholder='first of all'>")
+
+
+// let htt = $("h2[id=result]").html()
+// console.log(htt);
+
+
+// $("h2").remove()
+
+// $("h2").empty()
+
+
+// $("#second").before("<label>here you insert the second value</label><br>")
+
+
+// $("#app").addClass("yellow")
+// $("#samsg").addClass("purple")
+
+// $("#chinese").addClass("red")
+// $("#huawe").addClass("blue")
+
+// $("#korean").addClass("green")
+
+// $("#app").removeClass("yellow")
+
+
+
+// $("#samsg").css("background-color", "pink")
+
+
+// $("#samsg").css({
+//     "background-color": "violet",
+//     width: "300px",
+//     color: "white"
+
+// })
+
+
+// let ff = $("#second").val("1223")
+// console.log(ff);
+
+
+// $("#app").attr("class")
+
+
+// $("li").css("background-color", "pink")
+
+
+// console.log($("#second").val())
+
+
+// console.log(isNumeric("hi"))
+// console.log($.isNumeric("4"))
+
+
+// $("li").css("background-color", "pink")
+
+// $("ul").find("li").css("background-color", "pink")
+
+// console.log($("li").parents())
+
+// $("ul").children().css("height", "200px")
+// $("li").parent().css("background-color", "pink")
+// $("li").parent().css("background-color", "pink")
+
+
+// $("#samsg").next().css("background-color", "yellow")
+// $("#samsg").nextAll().css("background-color", "yellow")
+
+// $("#samsg").prev().css("background-color", "red")
+
+// $("#chinese").prevAll().css("background-color", "pink")
+
+
+// $("ul").find("li").css({"border": "2px solid green"})
+
+let parent = document.querySelector("#lists")
+let child = document.getElementById("samsg")
+
+console.log($.contains(parent, child));
+
+
+
+
+console.log($("ul").has("li").css({"border": "5px solid pink"}))
+
+
+
+
+
