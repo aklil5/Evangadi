2251eb6b5f45638aa8f26437b25f835c9cf02d34
aklil5 <aklilezewdu@gmail.com>
2026-08-10 22:35:03 +0300

    practiced using es6 module

--- Phase-3/Week-15/Examples/js/es6/file1.js
+++ Phase-3/Week-15/Examples/js/es6/file1.js
@@ -1,6 +1,18 @@
-// Using ES6 standard
-var variable1 = "ONE"
-var variable2 = "TWO"
+// // Using ES6 standard
+// var variable1 = "ONE"
+// var variable2 = "TWO"
 
-export default variable1;
-export {variable2};
+// export const add = (a, b) => a + b;
+
+
+// export default variable1;
+// export {variable2};
+
+
+
+let name = "aklile"
+let dept = "sw"
+
+
+export default name;
+export {dept};
--- Phase-3/Week-15/Examples/js/es6/test.js
+++ Phase-3/Week-15/Examples/js/es6/test.js
@@ -1,12 +1,18 @@
-// // USING ES6 MODULE 
-import variable1 from "./file1.js"
-console.log(variable1);
+// // // USING ES6 MODULE 
+// import variable1 from "./file1.js"
+// console.log(variable1);
 
 
-import { variable2 }  from "./file1.js";
-console.log(variable2);
+// import { variable2, add }  from "./file1.js";
+// console.log(variable2);
 
-// ORR
+// console.log(add(2, 3));
+// // ORR
 
-// import variable1, { variable2 } from "./file.js";
-// console.log(variable1, variable2);
+// // import variable1, { variable2 } from "./file.js";
+// // console.log(variable1, variable2);
+
+import name, {dept} from './file1.js'
+console.log(name);
+
+console.log(dept);
